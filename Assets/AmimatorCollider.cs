dac33737dceb92325a0817ba4908b22b6a39b92d
Blacksmith <michael.antoine.assaf@gmail.com>
2020-07-23 12:02:44 -0400

    beautiful collision collider detections, smoother animations

---
+++ Assets/AmimatorCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmimatorCollider : MonoBehaviour
+{
+    public GameObject bodyPart;
+    public SphereCollider sphereCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sphereCollider.transform.position = bodyPart.transform.position;
+    }
+}
