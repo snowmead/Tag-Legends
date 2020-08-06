527451db2eea85ce735ad51f1ac70afbeb2ba7bd
xcodesmith <xcodesmith@gmail.com>
2020-08-06 12:51:43 -0400

    fucking amazing animation handling

--- Assets/ShoutAnimationStateHandler.cs
+++ Assets/ShoutAnimationStateHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Shout : StateMachineBehaviour
+public class ShoutAnimationStateHandler : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
