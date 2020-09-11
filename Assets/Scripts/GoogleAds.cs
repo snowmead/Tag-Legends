b9fd48ac439cad1e4dc7557366a73e6f3a4752a6
Jesse Rafters <jesse.rafters@gmail.com>
2020-09-11 02:10:07 -0400

    Added Interstitial Ad

--- Assets/Scripts/GoogleAds.cs
+++ Assets/Scripts/GoogleAds.cs
@@ -7,6 +7,7 @@ using GoogleMobileAds.Api;
 public class GoogleAds : MonoBehaviour
 {
     private BannerView bannerView;
+    private InterstitialAd interstitial;
 
     public void Start()
     {
@@ -14,6 +15,7 @@ public class GoogleAds : MonoBehaviour
         MobileAds.Initialize(initStatus => { });
 
         this.RequestBanner();
+        this.RequestInterstitial();
     }
 
     private void RequestBanner()
@@ -46,6 +48,31 @@ public class GoogleAds : MonoBehaviour
         // Load the banner with the request.
         this.bannerView.LoadAd(request);
 
+    }
+    private void RequestInterstitial()
+    {
+        #if UNITY_ANDROID
+                string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        #elif UNITY_IPHONE
+                string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        #else
+                string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+
+    public void ShowInterstitialAd()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
     }
 
     public void HandleOnAdLoaded(object sender, EventArgs args)
