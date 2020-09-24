176833dff114c80566eaca2c9e66f8af1adc1d8a
xcodesmith <xcodesmith@gmail.com>
2020-09-24 16:15:09 -0400

    Banner view doesnt show after leave game

--- Assets/Scripts/GoogleAds.cs
+++ Assets/Scripts/GoogleAds.cs
@@ -13,40 +13,40 @@ public class GoogleAds : MonoBehaviour
     {
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(initStatus => { });
-
-        this.RequestBanner();
-        this.RequestInterstitial();
+        
+        RequestBanner();
+        RequestInterstitial();
     }
 
     private void RequestBanner()
     {
         #if UNITY_ANDROID
-                string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
         #elif UNITY_IPHONE
-                    string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
         #else
-                    string adUnitId = "unexpected_platform";
+            string adUnitId = "unexpected_platform";
         #endif
 
         // Create a 320x50 banner at the top of the screen.
-        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
 
         // Called when an ad request has successfully loaded.
-        this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
         // Called when an ad request failed to load.
-        this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
         // Called when an ad is clicked.
-        this.bannerView.OnAdOpening += this.HandleOnAdOpened;
+        bannerView.OnAdOpening += HandleOnAdOpened;
         // Called when the user returned from the app after an ad click.
-        this.bannerView.OnAdClosed += this.HandleOnAdClosed;
+        bannerView.OnAdClosed += HandleOnAdClosed;
         // Called when the ad click caused the user to leave the application.
-        this.bannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
+        bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
 
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
 
         // Load the banner with the request.
-        this.bannerView.LoadAd(request);
+        bannerView.LoadAd(request);
 
     }
     private void RequestInterstitial()
@@ -73,6 +73,11 @@ public class GoogleAds : MonoBehaviour
         {
             this.interstitial.Show();
         }
+    }
+
+    private void OnDestroy()
+    {
+        bannerView.Destroy();
     }
 
     public void HandleOnAdLoaded(object sender, EventArgs args)
