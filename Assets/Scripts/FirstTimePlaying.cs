5b22d45196931e46932a5190ab8f8ff48ff08275
xcodesmith <xcodesmith@gmail.com>
2020-09-24 22:15:40 -0400

    Added base tutorial foundation

---
+++ Assets/Scripts/FirstTimePlaying.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTimePlaying : MonoBehaviour
+{
+    public GameObject tutorial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("Tutorial") != 1)
+        {
+            PlayerPrefs.SetInt("Tutorial", 1);
+            tutorial.SetActive(true);
+        }
+    }
+}
